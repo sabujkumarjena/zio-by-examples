e6f39f7b6df2fc5bbdaf67d050a14286817295b9
sabuj <sabuj.kumar.jena@gmail.com>
2023-09-15 12:31:36 +0530

    ZIODependencies

---
+++ src/main/scala/com/allevite/zioByExamples/section1/ServiceModel.scala
@@ -0,0 +1,80 @@
+package com.allevite.zioByExamples.section1
+
+import zio._
+object ServiceModel {
+
+  // app to subscribe users to newsletter
+  case class User(name: String, email: String)
+
+  class UserSubscription(
+      emailService: EmailService,
+      userDatabase: UserDatabase
+  ) {
+    def subscribeUser(user: User): Task[Unit] =
+      for {
+        _ <- emailService.email(user)
+        _ <- userDatabase.insert(user)
+      } yield ()
+  }
+
+  object UserSubscription {
+    def create(emailService: EmailService, userDatabase: UserDatabase) =
+      new UserSubscription(emailService, userDatabase)
+
+    val live
+        : ZLayer[EmailService with UserDatabase, Nothing, UserSubscription] =
+      ZLayer.fromFunction(create _)
+  }
+
+  class EmailService {
+    def email(user: User): Task[Unit] =
+      ZIO.succeed(
+        println(
+          s"You've just been subscribed to Allevite. Welcome, ${user.name}!"
+        )
+      )
+  }
+
+  object EmailService {
+    def create(): EmailService = new EmailService
+
+    val live: ZLayer[Any, Nothing, EmailService] =
+      ZLayer.succeed(create())
+  }
+
+  class UserDatabase(connectionPool: ConnectionPool) {
+    def insert(user: User): Task[Unit] = for {
+      conn <- connectionPool.get
+      _ <- conn.runQuery(
+        s"insert into subscribers(name, email) values (${user.name}, ${user.email})"
+      )
+    } yield ()
+  }
+
+  object UserDatabase {
+    def create(connectionPool: ConnectionPool) =
+      new UserDatabase(connectionPool)
+
+    val live: ZLayer[ConnectionPool, Nothing, UserDatabase] =
+      ZLayer.fromFunction(create _)
+  }
+
+  class ConnectionPool(nConnections: Int) {
+    def get: Task[Connection] =
+      ZIO.succeed(println("Acquired connection")) *> ZIO.succeed(Connection())
+  }
+
+  object ConnectionPool {
+    def create(nConnections: Int) =
+      new ConnectionPool(nConnections)
+
+    def live(nConnections: Int): ZLayer[Any, Nothing, ConnectionPool] =
+      ZLayer.succeed(create(nConnections))
+  }
+
+  case class Connection() {
+    def runQuery(query: String): Task[Unit] =
+      ZIO.succeed(println(s"Executing query: $query"))
+  }
+
+}
